e490ae9bd4c05beb0fb396a134728f065cd22a2b
bbaustin <benjaminboydaustin@gmail.com>
2023-06-13 21:10:20 +0900

    Change vite config to deploy on different url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://hereismy.website/ship-battle/',
+  base: 'https://ship-battle.hereismy.website',
 });
